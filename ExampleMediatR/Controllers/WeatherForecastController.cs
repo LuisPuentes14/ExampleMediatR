4af046ff00a8100e2ae9ef82c5d45b4b32ecc9a5
AlejandroPuentes <alejandropuentes@wposs.com>
2024-06-06 11:37:01 -0500

    Add project files.

---
+++ ExampleMediatR/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleMediatR.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMediator _mediator;
+
+
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpPost]
+        //[Route("/{data}")]
+        public async Task<IActionResult> GetProcessedData([FromBody] MyQuery data)
+        {
+            var response = await _mediator.Send(data);
+            return Ok(response.ProcessedData);
+        }
+    }
+}
---
+++ ExampleMediatR/Example.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+
+namespace ExampleMediatR
+{
+    public class Example
+    {
+
+
+    }
+
+
+    public class MyQuery : IRequest<MyResponse>
+    {
+        public string Data { get; set; }
+    }
+
+    public class MyQueryHandler : IRequestHandler<MyQuery, MyResponse>
+    {
+        public async Task<MyResponse> Handle(MyQuery request, CancellationToken cancellationToken)
+        {
+            return new MyResponse { ProcessedData = $"Processed: {request.Data}" };
+        }
+    }
+
+
+    public class MyResponse
+    {
+        public string ProcessedData { get; set; }
+    }
+}
